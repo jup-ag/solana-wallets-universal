011f6bf7ba43233a3f24168c41dc26fb988990da
aidanaden <aidan@u.nus.edu>
2024-08-10 07:48:08 +0800

    test: remove default wallets

--- examples/start-tailwind/src/app.tsx
+++ examples/start-tailwind/src/app.tsx
@@ -1,10 +1,10 @@
 import { Router } from "@solidjs/router"
 import { FileRoutes } from "@solidjs/start/router"
-import { createEffect, Suspense } from "solid-js"
+import { Suspense } from "solid-js"
 import Nav from "~/components/Nav"
 import "./app.css"
-import { WalletProvider, Wallet } from "@solana-wallets-solid/core"
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
+import { WalletProvider } from "@solana-wallets-solid/core"
+// import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
 
 export default function App() {
   // const {
@@ -14,16 +14,16 @@ export default function App() {
   // SolletExtensionWalletAdapter,
   // TorusWalletAdapter,
   // } = await import('@solana/wallet-adapter-wallets');
-  const adapters = [new PhantomWalletAdapter()]
-  const wallets: Wallet[] = adapters.map(a => ({ adapter: a, readyState: a.readyState }))
-  createEffect(() => {
-    console.log({ wallets })
-  })
+  // const adapters = [new PhantomWalletAdapter()]
+  // const wallets: Wallet[] = adapters.map(a => ({ adapter: a, readyState: a.readyState }))
+  // createEffect(() => {
+  //   console.log({ wallets })
+  // })
   return (
     <Router
       root={props => (
         <>
-          <WalletProvider autoConnect={true} wallets={wallets}>
+          <WalletProvider autoConnect={true} wallets={[]}>
             <Nav />
             <Suspense>{props.children}</Suspense>
           </WalletProvider>
